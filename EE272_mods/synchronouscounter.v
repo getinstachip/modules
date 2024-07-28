0697ab0e6c7a6ee01e825a22c0a1d37a9ef942c5
Jagadeepram <86034408+Jag-M@users.noreply.github.com>
2024-07-27 21:51:54 -0400

    Create synchronouscounter.v

---
+++ EE272_mods/synchronouscounter.v
@@ -0,0 +1,13 @@
+module sync_counter #(parameter N = 4) (
+    input clk,
+    input reset,
+    output reg [N-1:0] q
+);
+
+    always @(posedge clk or posedge reset) begin
+        if (reset)
+            q <= 0;
+        else
+            q <= q + 1;
+    end
+endmodule
