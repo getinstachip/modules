3844a6f0a6ef46e3804cefba1474004dab1129e9
PVadde <pvadde@purdue.edu>
2024-07-29 22:56:09 +0530

    updated d-flip-flop redundancy

--- memory/dflipflop.v
+++ memory/dflipflop.v
@@ -1,9 +1,10 @@
 module dff(d,clk,q);
-    input d,clk;
-    output q;
-    reg q=0;
-    always @ (posedge clk)
-        begin
-            q<=d;
-        end
+    parameter N = 1;
+    output reg [N-1:0] q;
+    input [N-1:0] d;
+    input clk;
+
+    initial q = {N{1'b0}};
+    always @(posedge clk)
+        q <= d;
 endmodule
